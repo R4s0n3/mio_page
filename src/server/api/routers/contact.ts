e7b3c894b4af11d3d3159f60d45d1c8d71e7e3e2
r4s0n3 <github@miomideal.com>
2025-03-24 22:10:51 +0100

    contact.ts

--- src/server/api/routers/contact.ts
+++ src/server/api/routers/contact.ts
@@ -7,14 +7,21 @@ import {
     getCreators: publicProcedure.query(async ({ ctx }) => {
       const contacts = await ctx.db.contactLinks.findMany({
         where:{
-          user:{
-            email: {
-              in: ["pascal@miomideal.com", "heinachim@gmx.de"],
-            },
-            role: {
-              in:["ADMIO", "GORLAMI"]
-            }
-          }
+         OR: [
+      {
+        user: {
+          email: "pascal@miomideal.com",
+          role: "ADMIO"
+        }
+      },
+      {
+        user: {
+          email: "heinachim@gmx.de",
+          role: "ADMIO"
+        }
+      }
+    ]
+  },
         },
         include:{
           user:{
